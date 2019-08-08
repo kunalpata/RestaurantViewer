5ee91ff82a15ac9b08d316ba0f464cc0708010dc
Kunal Patadia <kunalpata@gmail.com>
2019-08-07 17:03:32 -0700

    Initial Commit

---
+++ RestaurantViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantViewer
+//
+//  Created by Kunal Patadia on 8/7/19.
+//  Copyright © 2019 Kunal Patadia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
